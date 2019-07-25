42a93036befc69aa69a583431a75cff160376c12
Danilo de Carvalho Souza <danilotremere@gmail.com>
2019-07-25 14:35:45 -0300

    Refactoring Search interface

--- src/app/services/search.ts
+++ src/app/services/search.ts
@@ -1,3 +1,11 @@
-export interface Search {
-  items: [];
+export default interface Search {
+  short_description: string;
+  description: string;
+  created_by: string;
+  released: string;
+  created_at: string;
+  updated_at: string;
+  featured: boolean;
+  curated: boolean;
+  score: number;
 }
